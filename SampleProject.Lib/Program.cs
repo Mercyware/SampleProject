f7244d52dd7daff471fceff3b40ac05e1f35ee28
Olaitan Adeboye <oladeboy@microsoft.com>
2021-04-08 17:15:26 +0100

    Initialize project

---
+++ SampleProject.Lib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleProject.Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
